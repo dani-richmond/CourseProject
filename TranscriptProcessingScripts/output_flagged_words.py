6bc1c00c86654110d79c5ba1d22a453ec7a9f9c8
CatherineHParker <71035088+CatherineHParker@users.noreply.github.com>
2021-11-29 18:36:42 -0800

    prototype changes

---
+++ TranscriptProcessingScripts/output_flagged_words.py
@@ -0,0 +1,19 @@
+#so now what?
+#I need to take the info from the typo_finder function in that .py file.
+#and output it in a meaningful way
+#and try to use the timestamp info as well
+# how do I get the info from the other file?
+# how do I get the timestamp info? use the dict that Dani made?
+# so for each word in the typo_list, output that and then...
+# go into the dict??
+
+#import packages and other files
+import re
+import os
+import math
+import typo_finder
+import unigram_testing
+
+# okay, now what?
+# where do I start?
+# I guess I go through the list of typos in typo_finder
